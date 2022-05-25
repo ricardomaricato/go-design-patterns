2c4f4354cb366a478bdeaf7fba4c55fd047519ef
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-05-25 09:40:26 -0300

    feat: liskov substitution principle

--- solid/liskov_substitution_principle/main.go
+++ solid/liskov_substitution_principle/main.go
@@ -2,7 +2,12 @@ package main
 
 import "fmt"
 
-type Sized interface {
+type Area interface {
+	GetArea() int
+}
+
+type IRectangle interface {
+	Area
 	GetWidth() int
 	SetWidth(width int)
 	GetHeight() int
@@ -14,6 +19,10 @@ type Rectangle struct {
 }
 
 //     vvv !! POINTER
+
+func (r *Rectangle) GetArea() int {
+	return r.width * r.height
+}
 
 func (r *Rectangle) GetWidth() int {
 	return r.width
@@ -58,27 +67,56 @@ func (s *Square) SetHeight(height int) {
 	s.height = height
 }
 
+type ISquare interface {
+	Area
+	SetSize(size int)
+	GetSize() int
+}
+
 type Square2 struct {
 	size int
+}
+
+func (s *Square2) GetArea() int {
+	return s.size * s.size
+}
+
+func (s *Square2) SetSize(size int) {
+	s.size = size
+}
+
+func (s *Square2) GetSize() int {
+	return s.size
 }
 
 func (s *Square2) Rectangle() Rectangle {
 	return Rectangle{s.size, s.size}
 }
+func SquareUseIt(sized ISquare) {
+	sized.SetSize(10)
+	width := sized.GetSize()
+	expectedArea := 10 * width
+	actualArea := sized.GetArea()
+	fmt.Print("Expected an area of ", expectedArea,
+		", but got ", actualArea, "\n")
+}
 
-func UseIt(sized Sized) {
+func RetangleUseIt(sized IRectangle) {
 	width := sized.GetWidth()
 	sized.SetHeight(10)
 	expectedArea := 10 * width
-	actualArea := sized.GetWidth() * sized.GetHeight()
+	actualArea := sized.GetArea()
 	fmt.Print("Expected an area of ", expectedArea,
 		", but got ", actualArea, "\n")
 }
 
 func main() {
 	rc := &Rectangle{2, 3}
-	UseIt(rc)
+	RetangleUseIt(rc)
 
 	sq := NewSquare(5)
-	UseIt(sq)
+	RetangleUseIt(sq)
+
+	sq2 := &Square2{30}
+	SquareUseIt(sq2)
 }
